e72e8a69a31dcba949086475d8149f84b59bcabc
boytech <32295343+bachtranuav@users.noreply.github.com>
2021-10-02 21:30:34 +0700

    init file

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐Heloo tục tưng !🖐',
+    introDesc: `Pách có điều này muốn hỏi Ngưn Ngưn, Ngưn dzớ phải chả nời thật lòng nha`,
+    btnIntro: 'OK',
+    title: 'Pách biết Ngưn thích Pách lắm phải không nào ? 😙',
+    desc: 'Ngưn không trả lời hoặc thoát ra, tức là Ngưn rất thích Pách đó nha :> ',
+    btnYes: 'Có, Ngưn thật sự rất thích anh Pách 💖💖💖',
+    btnNo: '💔 Pách mơ à ! 💔 ',
+    question:'Nói cho Pách biết đi vì sao Ngưn thích Pách điiiiii 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì anh Pách quá nhà đẹp trai đó Ahiiiii 😊😊',
+    mess: 'Chời chời. Quá là gõ gàng nuôn. Pách biết mà, Pách cũng thích Ngưn lắm 🥰',
+    messDesc: '💬💬 Call me nhoaz babae. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://m.me/b0y7ech'
+}
